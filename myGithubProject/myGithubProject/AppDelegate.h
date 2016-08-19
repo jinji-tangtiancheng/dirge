e9f548e4e82bc2747b2e8fbd928defadb502e89e
tangtiancheng <tangtianchengjinji-inc.com>
2016-08-19 16:00:39 +0800

    添加一个工程

---
+++ myGithubProject/myGithubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myGithubProject
+//
+//  Created by 唐天成 on 16/8/19.
+//  Copyright © 2016年 唐天成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myGithubProject/myGithubProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myGithubProject
+//
+//  Created by 唐天成 on 16/8/19.
+//  Copyright © 2016年 唐天成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
